6581e2fb0306bd59f86f410a27c8d11250ff8f51
Jockefiskaren <joakim.gourie@fiskenastet.se>
2025-05-05 11:02:29 +0200

    Add working API test route

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-// Express backend entry point
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("API is working");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
